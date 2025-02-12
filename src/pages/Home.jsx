cf5b01ab22ed04419e04e0f8cf1e34b9b86d1635
JhonF424 <jhonfranco424@gmail.com>
2025-02-11 20:23:24 -0500

    There is a new Home Page with a container

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Home() {
+    const [done, setDone] = useState({})
+
+    const onChange = (event) => {
+        // Handle onchange
+    }
+    // Handle UseEffect
+
+    return <main className="w-full">
+        <h1>Holi</h1>
+    </main>
+}
+
